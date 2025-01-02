172a44d5f5282eb30d18fefe86efe110c6c0146e
H. Elsner <48010199+h-elsner@users.noreply.github.com>
2025-01-02 15:50:22 +0100

    License added

--- exifstuff.pas
+++ exifstuff.pas
@@ -1,3 +1,37 @@
+{********************************************************}
+{                                                        }
+{       Common functions and definitions                 }
+{                                                        }
+{       Copyright (c) 2024-2025    Helmut Elsner         }
+{                                                        }
+{       Compiler: FPC 3.2.3   /    Lazarus 3.7           }
+{                                                        }
+{ Pascal programmers tend to plan ahead, they think      }
+{ before they type. We type a lot because of Pascal      }
+{ verboseness, but usually our code is right from the    }
+{ start. We end up typing less because we fix less bugs. }
+{           [Jorge Aldo G. de F. Junior]                 }
+{********************************************************}
+
+(*
+This source is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 2 of the License, or (at your option)
+any later version.
+
+This code is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+A copy of the GNU General Public License is available on the World Wide Web
+at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing
+to the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+Boston, MA 02110-1335, USA.
+
+*******************************************************************************)
+
+
 {Common EXIF definitions and routines
 
  Needed component: https://sourceforge.net/p/lazarus-ccr/svn/HEAD/tree/components/fpexif/
